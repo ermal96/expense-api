169650f12b2573ef4487d672b404902513fa7581
root <root@mail.altersoft.al>
2022-05-26 22:28:30 +0200

    fix: update config

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -14,16 +14,16 @@ interface IConfig {
 const NODE_ENV: string = process.env.NODE_ENV || 'development';
 
 const development: IConfig = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 9500,
     database: {
         MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/',
-        MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'example_db',
+        MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'expense',
     },
     secret: process.env.SECRET || 'secret',
 };
 
 const production: IConfig = {
-    port: process.env.PORT || 950,
+    port: process.env.PORT || 9500,
     database: {
         MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/',
         MONGODB_DB_MAIN: process.env.MONGODB_DB_MAIN || 'expense',
